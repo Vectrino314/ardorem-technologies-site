56eb3f1bff5afab918b6dde569f37acd2be1c54b
Victor Solis <victsoli@Victors-MacBook-Pro.local>
2019-05-03 17:47:49 -0600

    Added public folder

---
+++ public/proposal/payment-info.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale =1.0, user-scalable =no">
+ 
+<link href='https://betterproposals.io/proposal/custom.css' rel='stylesheet' type='text/css'>
+
+<title>Your Proposal</title>
+</head>
+
+<body>
+
+<div id="holder" style="width: 100%; height:100%; overflow:auto">
+<iframe id="iframe1" frameborder="0" src="https://betterproposals.io/proposal/payment-info.php?ProposalID=<?php echo $_GET['ProposalID'];?>&ContactID=<?php echo $_GET['ContactID'];?>" webkitallowfullscreen mozallowfullscreen allowfullscreen scrolling="yes"></iframe> 
+</div>
+
+</body>
+</html>
