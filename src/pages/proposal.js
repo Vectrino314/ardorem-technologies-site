4f9cb63679388a6a49d621ead56995c3b10eadd2
Victor Solis <victsoli@Victors-MacBook-Pro.local>
2019-05-05 23:28:15 -0600

    Test proposal page

--- src/pages/proposal.js
+++ src/pages/proposal.js
@@ -1,24 +1,17 @@
 import React from 'react'
 
+function createMarkup() {
+    return {__html: 'First &middot; Second'};
+  }
+  
+//   function MyComponent() {
+//     return <div dangerouslySetInnerHTML={createMarkup()} />;
+//   }
+
 export default () => (
-    // <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta charset="utf-8" />
-<meta name="robots" content="noindex, nofollow" />
-<meta name="viewport" content="width=device-width, initial-scale =1.0, user-scalable =no" />
 
-<link href='https://betterproposals.io/proposal/custom.css' rel='stylesheet' type='text/css' />
+    <div dangerouslySetInnerHTML={createMarkup()} />
 
-<title>Your Proposal</title>
-</head>
-
-<body>
-
-<iframe id="iframe1" frameborder="0" src="https://betterproposals.io/proposal/customdomain.html"></iframe> 
-
-</body>
-</html>
     // {/* // <html>
     // //     <script src="proposalFiles/cover.php"></script>
     // //     <script src="proposalFiles/forward.php"></script>
