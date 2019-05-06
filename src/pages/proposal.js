ed0a9ce0dd3ec372b745f3e61d9586934c12f497
Victor Solis <victsoli@Victors-MacBook-Pro.local>
2019-05-05 22:51:03 -0600

    Fixed Proposal Page

--- src/pages/proposal.js
+++ src/pages/proposal.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
 export default () => (
-    <html>
-        <script src="proposalFiles/cover.php"></script>
-        <script src="proposalFiles/forward.php"></script>
-        <script src="proposalFiles/index.php"></script>
-        <script src="proposalFiles/payment-info.php"></script>
-        <script src="proposalFiles/payment.php"></script>
-        <script src="success.php"></script>
-        <script src="proposalFiles/thanks.php"></script>
-    </html>
+    // <!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8" />
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="width=device-width, initial-scale =1.0, user-scalable =no" />
+
+<link href='https://betterproposals.io/proposal/custom.css' rel='stylesheet' type='text/css' />
+
+<title>Your Proposal</title>
+</head>
+
+<body>
+
+<iframe id="iframe1" frameborder="0" src="https://betterproposals.io/proposal/customdomain.html"></iframe> 
+
+</body>
+</html>
+    // {/* // <html>
+    // //     <script src="proposalFiles/cover.php"></script>
+    // //     <script src="proposalFiles/forward.php"></script>
+    // //     <script src="proposalFiles/index.php"></script>
+    // //     <script src="proposalFiles/payment-info.php"></script>
+    // //     <script src="proposalFiles/payment.php"></script>
+    // //     <script src="success.php"></script>
+    // //     <script src="proposalFiles/thanks.php"></script>
+    // // </html> */}
 )
